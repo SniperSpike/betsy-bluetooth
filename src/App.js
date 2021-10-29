99f06c67f2b7d953c0974bd292bfa6ac8c5f83d8
SniperSpike <joyvroolanddnv@gmail.com>
2021-10-29 10:52:21 +0200

    v1.3.5

--- src/App.js
+++ src/App.js
@@ -29,15 +29,6 @@ function App(params) {
     }
   }
 
-  window.addEventListener("beforeinstallprompt", (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    showInstallPromotion();
-  });
-
   return (
     <Router>
       <div className="App">
