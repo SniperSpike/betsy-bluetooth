6abfeb98a90b34dad5ca09fd23fb49753cad5a30
SniperSpike <joyvroolanddnv@gmail.com>
2021-10-29 10:25:28 +0200

    v1.3.4

--- src/App.js
+++ src/App.js
@@ -22,11 +22,21 @@ function App(params) {
   const dispatch = useDispatch();
   const getUser = useSelector((state) => state.user);
   const [user] = useAuthState(auth);
+  let deferredPrompt;
   if (getUser !== user) {
     if (user !== null) {
       dispatch(setUser(user));
     }
   }
+
+  window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+  });
 
   return (
     <Router>
